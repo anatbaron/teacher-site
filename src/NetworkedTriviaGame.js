0a60de5c97a345b0146e84ea4483c3608b70981f
anatbaron <anatbaron@outlook.com>
2025-02-17 02:11:50 +0200

    Update NetworkedTriviaGame.js

--- src/NetworkedTriviaGame.js
+++ src/NetworkedTriviaGame.js
@@ -149,7 +149,7 @@ const NetworkedTriviaGame = () => {
   
     // Initialize Socket Connection
     useEffect(() => {
-      const newSocket = io('trivia-reactjs-server.vercel.app', {
+      const newSocket = io('https://trivia-reactjs-server.glitch.me', {
         headers:{
           "user-agent": "chrome"
         }
